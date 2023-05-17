534fc3c1872e175d1a1be5ac7324b993c02ecea6
Cyril <cdldl@connect.ust.hk>
2023-05-17 10:51:11 +0800

    Updated backtest

--- R_in_Finance_2023_backtest.R
+++ R_in_Finance_2023_backtest.R
@@ -7,11 +7,11 @@ lapply(list.of.packages, function(x) require(x, character.only = TRUE))
 
 # CONSTANTS
 cores = detectCores()-2
-delta_t = 1
+delta_t = 6
 multiplier = 100
-n_days_in_a_month=21
+n_days_in_a_month=delta_t/21
 option_files_path ='/home/cyril/TRADING/ORATS/data/'
-option_path_output = "/home/cyril/TRADING/ORATS/RinFinance/"
+option_path_output = "/home/cyril/TRADING/ORATS/RinFinance6/"
 dir.create(option_path_output)
 source(file = "/home/cyril/RinFinance/R_in_Finance_2023_functions.R")
 
@@ -30,7 +30,7 @@ stock = fread('/home/cyril/RinFinance/stock_wpreds.csv')
 vol[,date:=as.IDate(forecast_time + 8*60*60)]
 vol = vol[order(date)]
 vol[,name:=identifier]
-vol[,preds_vol:=shift(get(paste0('t+0')),1,type='lead'),by=name]
+vol[,preds_vol:=shift(get(paste0('t+', delta_t -1)),1,type='lead'),by=name]
 vol[,expected_volatility:=exp(preds_vol)]
 vol[,c('asset','maturity','delta'):=tstrsplit(identifier, '__',type.convert=T)]
 vol[,type:=fifelse(delta > 0,'call','put')]
@@ -44,10 +44,10 @@ option_files = list.files(option_files_path)
 test= substr(do.call(rbind,strsplit(option_files,'_|.csv'))[,4],1,4)
 option_files = option_files[test >= 2017 & test < 2020]
 
-option_files_lag = option_files[1:(length(option_files)-1)]
-option_files = option_files[2:length(option_files)]
+option_files_lag = option_files[1:(length(option_files)-delta_t)]
+option_files = option_files[(1+delta_t):length(option_files)]
 
-grid.param = cbind(option_files_lag,option_files) # [1:5,1:2] 
+grid.param = cbind(option_files_lag,option_files) #[1:2,1:2] 
 
 fe <- foreach(param = iter(grid.param, by = "row"),
               .verbose = TRUE, .errorhandling = "pass",
@@ -59,8 +59,9 @@ results <- fe %dopar% {
   option_files_lag = as.character(param[,1])
   option_file = as.character(param[,2])
   
-  # option_files_lag = grid.param[1,1]
-  # option_file = grid.param[1,2]
+  #FIXME
+  #option_files_lag = grid.param[1,1]
+  #option_file = grid.param[1,2]
 
   # LOAD OPTIONS
   old_options = fread(paste0(option_files_path,option_files_lag))
@@ -79,15 +80,18 @@ results <- fe %dopar% {
   options[,delta_original_call:=shift(delta,1,type='lag'),by=c('asset','strike','expirDate')]
   options[,delta_original_put:=shift(1-delta,1,type='lag'),by=c('asset','strike','expirDate')]
   
-  # SUBSET TO TODAY'S OPTIONS
-  options = options[tmp_date == trade_date]
-  
   # CONCATENATE OPTIONS WITH STOCK PREDICTIONS
   options[,year_month:=format(date[1],'%Y-%m'),by=date]
   stock[,year_month:=format(date,'%Y-%m')]
   stock[,asset:=symbol]
   options = merge(options, stock[,c('me','year_month','asset','preds'),with=F],
                   by=c('year_month','asset'))
+  options[,expected_stock_price:=(shift(stkPx,1,type='lag') * (1+ (preds *n_days_in_a_month))),
+          by=c('asset','strike','expirDate')]
+  
+  # SUBSET TO TODAY'S OPTIONS
+  options = options[tmp_date == trade_date]
+  
   
   # CONCATENATE OPTIONS WITH VOL PREDICTIONS
   deltas = sort(unique(vol_calls$delta))
@@ -104,41 +108,39 @@ results <- fe %dopar% {
                   by=c('date','asset','delta','maturity'))
   
   # COMPOUND OPTION INPUTS
-  options[,expected_maturity:=round((yte*365) - delta_t)/365]
-  options[,expected_stock_price:=(shift(stkPx,1,type='lag') * (1+preds/n_days_in_a_month))]
-    
+  options[,expected_maturity:=yte]
+  options = options[order(date)]
+  
   # COMPOUND EXPECTED PRICE OPTIONS
   options = option_pricing(options, delta_t)
   
   # COMPOUND RETURNS
-  options[,expected_returns_call:=(option_price_pred_call1 - lag_call_ask)/lag_call_ask]
-  options[,expected_returns_put:=(option_price_pred_put1 - lag_put_ask)/lag_put_ask]
+  options[,expected_returns_call:=(get(paste0('option_price_pred_call',delta_t)) - lag_call_ask)/lag_call_ask]
+  options[,expected_returns_put:=(get(paste0('option_price_pred_put',delta_t)) - lag_put_ask)/lag_put_ask]
   options[,real_returns_call:=(cBidPx - lag_call_ask)/lag_call_ask]
   options[,real_returns_put:=(pBidPx - lag_put_ask)/lag_put_ask]
   
-  # SET CALLS AND PUTS WITH THRESHOLD GIVEN
+  # SET CALLS AND PUTS WITH POSITIVE RETURNS
   calls = options[expected_returns_call >0.]
   calls[,delta:=delta_original_call]
   calls[,real_returns:=fifelse(!is.finite(real_returns_call),0,real_returns_call)]
+  calls[,type:='call']
+  calls[,margin:=lag_call_ask*multiplier]
+  calls[,expected_returns:=expected_returns_call]
+  calls[,spread:=cAskPx - cBidPx]
   
   puts = options[expected_returns_put > 0.]
   puts[,delta:=delta_original_put]
   puts[,real_returns:=fifelse(!is.finite(real_returns_put),0,real_returns_put)]
-
-  # SET VARS FOR ANALYSIS
-  calls[,margin:=lag_call_ask*multiplier]
-  calls[,expected_returns:=expected_returns_call]
+  puts[,type:='put']
   puts[,margin:=lag_put_ask*multiplier]
   puts[,expected_returns:=expected_returns_put]
+  puts[,spread:=pAskPx - pBidPx]
+  
   trading = rbind(calls,puts)
   
-  fwrite(trading,paste0(option_path_output,trading$date[1],'.csv'))  
+  fwrite(trading,paste0(option_path_output,options$date[1],'.csv'))  
   return(NULL)
 }
 
-
-
-
-
-
-
+print(results)
--- R_in_Finance_2023_trading.R
+++ R_in_Finance_2023_trading.R
@@ -6,42 +6,96 @@ if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, function(x) require(x, character.only = TRUE))
 
 # CONSTANTS
+delta_t = 6
 initial_capital = 1e4
 multiplier = 100
 riskfree = (1+0.0165)^(1/12) -1
-option_path_output = "/home/cyril/TRADING/ORATS/RinFinance/"
+option_path_output = "/home/cyril/TRADING/ORATS/RinFinance6/"
 benchmark_path='/home/cyril/TRADING/EOD/data/US/XLK.US.csv'
 source(file = "/home/cyril/RinFinance/R_in_Finance_2023_functions.R")
 
 # LOAD OPTIONS 
 files = list.files(option_path_output, full.names = T)
-options = NULL
-for(file in files) {
-  tmp_file = fread(file)
-  options = rbind(options,tmp_file)
-}
-options[,type:=fifelse(expected_returns_call>0,'call','put')]
+tmp_files = lapply(files,fread)
+to_remove = sapply(tmp_files,function(x) nrow(x) == 0)
+whi = which(to_remove == TRUE)
+if(length(whi) != 0) {
+  tmp_files = tmp_files[-whi]
+} 
+options = rbindlist(tmp_files)
+options[,midpoint:=fifelse(type=='call',(cAskPx + cBidPx)/2,(pAskPx + pBidPx)/2)]
+
+# LOAD STOCK DATA
+data = fread('/home/cyril/code/usa_filtered.csv')
+data[,asset:=symbol]
+data[,date:=paste0(substr(as.character(date),1,4),'-',
+                   substr(as.character(date),5,6),'-',
+                   substr(as.character(date),7,8))]
+data[,date:=fastPOSIXct(date, 'UTC')]
+data = data[order(date)]
+data[,year_month:=format(date,'%Y-%m')]
+options[,year_month:=format(date,'%Y-%m')]
+options2 = merge(options,data[,c('year_month','asset','me'),with=F],by=c('year_month','asset'))
+options = copy(options2)
 
 # METRICS TRADING
+options[,expected_returns:=fifelse(!is.finite(expected_returns),0,expected_returns)]
+options[,real_returns:=fifelse(!is.finite(real_returns),0,real_returns)]
+
 val = options[year(date) < 2018]
-cut_exp_ret = cut(val$expected_returns,
-                  unique(quantile(val$expected_returns, seq(0,1,0.05))))
+cut_exp_ret = cut(val$expected_returns,unique(quantile(val$expected_returns, seq(0,1,0.1))))
 cut_exp_delta = cut(val$delta,seq(0,1,0.2))
-cut_exp_mat = cut(val$maturity,unique(quantile(val$maturity, seq(0,1,0.2))))
+cut_exp_mat = cut(val$maturity,unique(quantile(val$maturity, seq(0,1,0.1))))
 cut_exp_margin = cut(val$margin,unique(quantile(val$margin,seq(0,1,0.2))))
-print(tapply(val$real_returns, list(cut_exp_ret,cut_exp_delta,cut_exp_margin),mean,na.rm=T)) 
+cut_exp_me = cut(val$me.y,unique(quantile(val$me.y,seq(0,1,0.2))))
+cut_exp_midpoint = cut(val$midpoint,unique(quantile(val$midpoint,seq(0,1,0.2))))
+cut_exp_spread = cut(val$spread,unique(quantile(val$spread,seq(0,1,0.2))))
+print(tapply(val$real_returns, list(cut_exp_ret,cut_exp_delta,val$type),sum,na.rm=T)) 
 
 # METRICS ABOVE MADE ME SELECT THESE THRESHOLDS
-test = options[year(date)>= 2018 & expected_returns >0.8  & delta > 0.35 &
-                 delta < 0.65 & maturity < 0.16 & margin > 250 & margin < 1000] 
+test = options[year(date) >= 2018  & expected_returns>0.35 & delta<0.6 & 
+                 maturity < 0.16 & spread > 0.05 & midpoint > 0.5]
+
+cut_exp_ret = cut(test$expected_returns,unique(quantile(test$expected_returns, seq(0,1,0.1))))
+cut_exp_delta = cut(test$delta,seq(0,1,0.2))
+print(tapply(test$real_returns, list(cut_exp_ret,cut_exp_delta,test$type),sum,na.rm=T)) 
 
 # INVEST DAILY A MAXIMUM OF YOUR INITIAL CAPITAL (FIXED CAPITAL OVER TIME)
 trading = test[order(-expected_returns),.SD,by=date]
 trading[,cum_margin:=cumsum(margin),by=date]
-trading2 = trading[cum_margin <= initial_capital]
+trading[,initial_capital:=initial_capital]
+whi = which(trading$initial_capital <= trading$initial_capital)
+trading2 =trading[whi]
+
+# BASIC PERFORMANCE WITHOUT AGGREGATION AND DAILY AGGREGATION
+trading2[,ret:=real_returns/multiplier]
+trading2 = trading2[order(date)]
+trading2[,pnl:=fifelse(type=='call', cBidPx - lag_call_ask,pBidPx - lag_put_ask)]
+# trading2 = benchmark[date>min(trading$date)& date < max(trading$date)]
+# trading2[,ret:=benchmark]
+data.table(
+  Starting.Period = first(trading2$date),
+  Ending.Period = last(trading2$date),
+  pnl= sum(trading2$ret),
+  n.trades=nrow(trading2),
+  pip.trade = mean(trading2$ret,na.rm=T)* 10000,
+  win.ratio.strat= (length(which(trading2$ret>0))/length(trading2$ret))*100,
+  sharpe= (mean(trading2$ret,na.rm=T)* sqrt(252/delta_t)) / sd(trading2$ret,na.rm=T)
+)
+
+trading3 = trading2[,list(ret=mean(real_returns,na.rm=T)/multiplier,
+                          pnl=sum(fifelse(type=='call', cBidPx - lag_call_ask,
+                                          pBidPx - lag_put_ask))),by=date]
+trading3 = copy(trading2)
+data.table(
+  pnl= sum(trading3$ret),
+  pip.trade = mean(trading3$ret,na.rm=T)* 10000,
+  win.ratio.strat= (length(which(trading3$ret>0))/length(trading3$ret))*100,
+  sharpe= (mean(trading3$ret,na.rm=T)* sqrt(252/delta_t)) / sd(trading3$ret,na.rm=T),
+  MDD=maxDrawdown(xts(trading3$ret,order.by=as.Date(trading3$date)))
+)
 
 # PERFORMANCE AGAINST BENCHMARK
-trading3 = trading2[,list(ret=mean(real_returns)/multiplier),by=date]
 benchmark =fread(benchmark_path)
 benchmark[,benchmark:=(Adjusted_close - shift(Adjusted_close,1,type='lag'))/
             shift(Adjusted_close,1,type='lag')]
